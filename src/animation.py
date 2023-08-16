86f259ae9ca4bc3b3a9644516fb9141c95c0a493
Shamsi Abdurakhmanova <sham.abdurakh@gmail.com>
2023-08-16 19:36:26 +0300

    add animation

--- src/animation.py
+++ src/animation.py
@@ -3,7 +3,11 @@
 from generate_data import get_shared_data
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import animation
 from pytorch_models import Linreg_Torch, MLP_Torch
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+
  
 def simulate_simpson(n_ds=3, n_samples=100, r=0.6, difference=3):
 
@@ -79,15 +83,22 @@ def train(G, A, iters=1000, regularizer_term=0.01, verbose=False):
     nodes_preds = np.zeros((m_shared, n_nodes))       # predictions on a shared ds
     preds_plot  = np.zeros((iters, n_nodes, m_plot))  # predictions on a dataset for plotting
 
+    # Scale training data
+    scalers = [StandardScaler(), StandardScaler(), StandardScaler()]
+    for node, scaler in zip(G, scalers):
+        node["ds_train"] = (scaler.fit_transform(node["ds_train"][0]), node["ds_train"][1])
+        node["ds_plot"]  = scaler.transform(node["ds_plot"])
+
     for i in range(iters):
-        print(f"Iteration {i+1}")
-        # Update local models
+        if verbose:
+            print(f"Iteration {i+1}")
+        
         for n in range(n_nodes):
             ds_train  = G[n]["ds_train"]
             x_plot    = G[n]["ds_plot"]
             ds_shared = G[n]["ds_shared"]
             model     = G[n]["model"]
-            model.update(ds_train, ds_shared, nodes_preds, A[n], regularizer_term) 
+            model.update(ds_train, ds_shared, nodes_preds, A[n], regularizer_term) # Update params of local models   
 
         # Update predictions on a shared test set 
         nodes_preds = np.zeros((m_shared, n_nodes)) 
@@ -109,6 +120,15 @@ def train(G, A, iters=1000, regularizer_term=0.01, verbose=False):
         
     return preds_plot
 
+# Params datasets
+N_FEATURES = 1
+N_SAMPLES  = 100
+
+# Params training
+LR    = 0.03
+ITERS = 1000
+REG   = 0
+
 # Create Simpson's datasets consisting of 3 local datasets with sample size 100
 ds_train, ds_plot = simulate_simpson()
 print(f'Shape of ds_train dataset feature matrix {ds_train[0][0].shape}')
@@ -116,55 +136,38 @@ print(f'Shape of ds_train dataset label vector {ds_train[0][1].shape}')
 print(f'Shape of ds_plot dataset {ds_plot[0].shape}')
 
 # Create shared dataset
-ds_shared = get_shared_data(n_samples=100, n_features=1)
-LR = 0.01
-models = [Linreg_Torch(n_features=1, lr=0.01), Linreg_Torch(n_features=1, lr=0.05), Linreg_Torch(n_features=1, lr=0.025)]
-print(models)
+ds_shared = get_shared_data(n_samples=N_SAMPLES, n_features=N_FEATURES)
+models = [Linreg_Torch(n_features=N_FEATURES, lr=LR) for i in range(3)]
 G, A = build_simpson_graph(ds_train, ds_plot, ds_shared, models)
 
-preds_plot = train(G, A, iters=2000, regularizer_term=0.05, verbose=True)
-print(preds_plot.shape)
+preds_plot = train(G, A, iters=ITERS, regularizer_term=REG)
 
 fig, ax = plt.subplots(1, 1, figsize=(10,8))
-for ds_pred in preds_plot[-10:]:
-    for ds, x_plot, y_plot in zip(ds_train, ds_plot, ds_pred):
-        ax.scatter(ds[0], ds[1])
-        ax.plot(x_plot, y_plot)
-plt.show()
 
-# #---------------------------------------------------#
-# import numpy as np
-# from sklearn.linear_model import LinearRegression
+ln1, = ax.plot([], [], 'k')
+ln2, = ax.plot([], [], 'k')
+ln3, = ax.plot([], [], 'k')
 
-# fig, ax = plt.subplots(1, 1, figsize=(10,8))
-# for ds, x_plot in zip(ds_train, ds_plot):
-#     X = ds[0]
-#     y = ds[1].reshape(-1,)
+lns = [ln1, ln2, ln3]
 
-#     reg = LinearRegression().fit(X, y)
-#     y_plot = reg.predict(x_plot)
+for ds, x_plot in zip(ds_train, ds_plot):
+    X = ds[0]
+    y = ds[1].reshape(-1,)
 
-#     print("params: ", reg.coef_, reg.intercept_)
+    reg = LinearRegression().fit(X, y)
+    y_plot = reg.predict(x_plot)
 
-#     ax.scatter(X, y)
-#     ax.plot(x_plot, y_plot)
-# plt.show()
+    ax.plot(x_plot, y_plot, 'k--')
+    ax.scatter(X, y)
 
-# # animation function.  This is called sequentially
-# def animate(i): 
-    
-#     for j in range(len(history_camel)):
-#         x_scatter = history_camel[j][:i,0]
-#         y_scatter = history_camel[j][:i,1]
-#         z_scatter = camel(x_scatter, y_scatter)
-
-#         ax1.plot(x_scatter,y_scatter,z_scatter,colors[j],label=labels[j])  
+def animate(i): 
+    ds_pred = preds_plot[i]
+    for x_plot, y_plot, ln in zip(ds_plot, ds_pred, lns):
+        ln.set_data(x_plot, y_plot)
   
-#     return ax1
+    return ax
 
+# call the animator.  blit=True means only re-draw the parts that have changed.
+anim = animation.FuncAnimation(fig, animate, frames=500, interval=10)
 
-# # call the animator.  blit=True means only re-draw the parts that have changed.
-# anim = animation.FuncAnimation(fig, animate, frames=500, interval=10)
-
-# anim.save('camel3D.mp4', fps=15)
-# plt.show()
+anim.save('src/simpsons.mp4', fps=25)
