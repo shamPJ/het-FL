d21346d519de16b973881f0fc8a17445727f45e9
Shamsi Abdurakhmanova <sham.abdurakh@gmail.com>
2024-06-18 10:10:49 +0300

    del extra out/ files

--- src/plot.py
+++ src/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 from utils import load_and_plot_mse, load_and_plot_est_error
 
-p_dir = os.getcwd() + "/out/Linreg_Torch_637015"
+p_dir = os.getcwd() + "/out/Linreg_Torch_665413"
 load_and_plot_mse(p_dir)
 load_and_plot_mse(p_dir, scaled=True)
 load_and_plot_est_error(p_dir)
