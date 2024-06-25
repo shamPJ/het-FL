3894d7df61e3bd8d60fe5387cccec484576a4536
Shamsi Abdurakhmanova <sham.abdurakh@gmail.com>
2024-06-25 15:24:14 +0300

    plot

--- src/plot.py
+++ src/plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 from utils import load_and_plot_mse, load_and_plot_est_error
 
-p_dir = os.getcwd() + "/out/Linreg_Torch_931647"
+p_dir = os.getcwd() + "/out/Linreg_Torch_984613"
 load_and_plot_mse(p_dir)
 load_and_plot_mse(p_dir, scaled=True)
 load_and_plot_est_error(p_dir)
